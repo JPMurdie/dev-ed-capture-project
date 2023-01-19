57d54dc8d04ca0fe4cce2e49b48d3c7a51db7d2a
Jason Murdie <jpmurdie@gmail.com>
2023-01-19 07:43:42 +0000

    Created and styled Nav.js then imported it into App.js.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 //Global Style
 import GlobalStyle from "./components/GlobalStyle";
+//Import Nav
+import Nav from "./components/Nav";
 //Import Pages
 import AboutUs from "./pages/AboutUs";
 
@@ -8,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
